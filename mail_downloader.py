944df43d034ff27fb7a891ffcb4c43d2ca9f49df
Simple Code Sample <simplecodesample@tutanota.com>
2022-04-14 18:18:06 +0530

    Add loop to cycle through each message and print the 'subject'. Then close connection to server

--- mail_downloader.py
+++ mail_downloader.py
@@ -21,3 +21,19 @@ imap.select(mail_box)
 
 # Email filtering
 _, mail_index = imap.search(None,"ALL")
+
+
+
+# Loop all the mails in mail_box
+for index in mail_index[0].split():
+    _, mail_data = imap.fetch(index,"(RFC822)")
+    email = email.message_from_bytes(mail_data[0][1])
+
+    # Print the subject of each mail
+    print(mail.get("Subject"))
+
+
+
+# Close and log out from server
+imap.close()
+imap.logout()
